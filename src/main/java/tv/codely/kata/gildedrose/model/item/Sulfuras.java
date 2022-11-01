be48fdf64c048a224202b2aefd41ee1e5d8c622f
Toni Peraira <toniperairam@gmail.com>
2022-11-01 19:42:39 +0100

    [my-solution-02-solid] Split items in classes that extend from `ItemUpdatable`
    `ItemUpdatable` implements the new `Item` interface with the `update` method.
    
    Change tests to work with the new structure.

---
+++ src/main/java/tv/codely/kata/gildedrose/model/item/Sulfuras.java
@@ -0,0 +1,13 @@
+package tv.codely.kata.gildedrose.model.item;
+
+public final class Sulfuras extends ItemUpdatable {
+
+  public Sulfuras(final String name, final int sellIn, final int quality) {
+    super(name, sellIn, quality);
+  }
+
+  @Override
+  public void update() {
+
+  }
+}
