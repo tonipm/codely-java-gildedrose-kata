82b783d13f55f7bfa773ce8b78f9523fe9803672
Toni Peraira <toniperairam@gmail.com>
2022-11-02 10:23:35 +0100

    [my-solution-02-solid] Add `ItemLegendary`
    A legendary item has a Quality that never alters. `Sulfuras` is a legendary item.

---
+++ src/main/java/tv/codely/kata/gildedrose/model/item/ItemLegendary.java
@@ -0,0 +1,11 @@
+package tv.codely.kata.gildedrose.model.item;
+
+class ItemLegendary extends ItemUpdatable {
+
+  public ItemLegendary(String name, int sellIn, int quality) {
+    super(name, sellIn, quality);
+  }
+
+  @Override
+  public void update() {}
+}
