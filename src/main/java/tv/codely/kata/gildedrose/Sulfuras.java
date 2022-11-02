c55690795ccf707f968e758f9020e66d64f7253b
Toni Peraira <toniperairam@gmail.com>
2022-11-02 11:11:53 +0100

    [my-solution-03-ubiquitous] Rename `Item` classes

--- src/main/java/tv/codely/kata/gildedrose/Sulfuras.java
+++ src/main/java/tv/codely/kata/gildedrose/Sulfuras.java
@@ -1,6 +1,6 @@
 package tv.codely.kata.gildedrose;
 
-final class Sulfuras extends ItemLegendary {
+final class Sulfuras extends LegendaryItem {
 
   Sulfuras(final ItemName name, final ItemSellIn sellIn, final ItemQuality quality) {
     super(name, sellIn, quality);
